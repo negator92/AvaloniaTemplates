f6443691c7e78971c3cfa357077cae9b7e0736b6
Vincent Neparidze <negator92@gmail.com>
2020-01-21 08:47:15 +0300

    Initial commit

---
+++ AvaloniaMVVMApplication/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvaloniaMVVMApplication.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ AvaloniaMVVMApplication/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace AvaloniaMVVMApplication.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
